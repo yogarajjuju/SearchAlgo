3ac0ccb95f6f6352ed51be5c62da4899fc96e916
Yogaraj <yogarajjuju22@gmail.com>
2025-03-17 11:31:02 +0530

    bankaii

---
+++ src/Celing.java
@@ -0,0 +1,26 @@
+public class Celing {
+    public static void main(String[] args) {
+        int [] arr  ={2,3,5,7,9,13,14,16,18};
+        int target = 15;
+        int ans = celing(arr, target);
+        System.out.println(ans);
+
+    }
+    static int celing (int [] arr ,int target){
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = start + (end - start) /2;
+            if(target<arr[mid]){
+                end = mid -1;
+            } else if (target>arr[mid]) {
+                start = mid +1;
+
+            } else {
+                return mid;
+            }
+
+        }
+        return start;
+    }
+}
