f60cd01affba5c57618b193d2bebc2af4b12f9ed
Yogaraj <yogarajjuju22@gmail.com>
2025-04-02 14:18:23 +0530

    bankaii

---
+++ src/Duplicate.java
@@ -0,0 +1,21 @@
+class Duplicate{
+    public int findDuplicate(int[] nums) {
+        int left = 0, right = nums.length-1;
+        while(left<right){
+            int mid = left +(right -left)/2;
+            int count = 0;
+            for(int num:nums){
+                if(num<=mid){
+                    count++;
+                }
+
+            }
+            if(count>mid){
+                right = mid;
+            } else {
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+}
