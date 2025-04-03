d677d4d6e93a5004acf914848b754cddea8c0980
Yogaraj <yogarajjuju22@gmail.com>
2025-04-03 18:43:13 +0530

    bankaii

---
+++ src/ArrayII.java
@@ -0,0 +1,18 @@
+class ArrayII {
+    public int findMin(int[] nums) {
+        int low = 0, high = nums.length-1;
+        while(low<high){
+            int mid = low+(high - low)/2;
+            if (nums[mid] > nums[high]){
+                low = mid +1;
+
+            } else if(nums[mid]<nums[low]){
+                high = mid;
+            } else{
+                high--;
+
+            }
+        }
+        return nums[ low];
+    }
+}
