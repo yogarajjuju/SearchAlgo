61382006b545a28e528e9c4cae7f31d9d9c66584
Yogaraj <yogarajjuju22@gmail.com>
2025-04-04 18:46:11 +0530

    bankaii

---
+++ src/Countof.java
@@ -0,0 +1,26 @@
+class Countof {
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer> sortedList = new ArrayList<>();
+        for(int i = nums.length-1; i>=0;i--){
+            int index = bs(sortedList,nums[i]);
+            result.add(index);
+            sortedList.add(index,nums[i]);
+        }
+        Collections.reverse(result);
+        return result;
+
+    }
+    public int bs(List<Integer> sortedList,int num){
+        int left= 0, right = sortedList.size();
+        while(left<right){
+            int mid = left+(right-left)/2;
+            if(sortedList.get(mid)>=num){
+                right  = mid;
+            } else{
+                left = mid+1;
+            }
+        }
+        return left;
+    }
+}
